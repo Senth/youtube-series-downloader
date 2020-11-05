d6f698c4f2a71b7731f80688e38ee1ce34e79a50
Matteus Magnusson <senth.wallace@gmail.com>
2020-11-05 15:34:04 +0100

    ♻ Remove channel name

--- youtube_series_downloader/video.py
+++ youtube_series_downloader/video.py
@@ -1,6 +1,5 @@
 class Video:
-    def __init__(self, id: str, date: str, title: str, channel_name: str):
+    def __init__(self, id: str, date: str, title: str):
         self.id = id
         self.date = date
         self.title = title
-        self.channel_name = channel_name

c462a3bb78541b2c2d4a34e74c756640bad259f5
Matteus Magnusson <senth.wallace@gmail.com>
2020-11-05 14:34:54 +0100

    ✨ Logger

---
+++ youtube_series_downloader/logger.py
@@ -0,0 +1,10 @@
+from .config import config
+
+def log_message(message: str):
+    """Log message if verbose has been set to true
+
+    Args:
+        message (str): The message to log
+    """
+    if config.verbose:
+        print(message)

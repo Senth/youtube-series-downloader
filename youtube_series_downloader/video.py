f44bc86435368f41107c264d4978fc01790e288d
Matteus Magnusson <senth.wallace@gmail.com>
2020-11-05 14:35:26 +0100

    ✨ Video

---
+++ youtube_series_downloader/video.py
@@ -0,0 +1,6 @@
+class Video:
+    def __init__(self, id: str, date: str, title: str, channel_name: str):
+        self.id = id
+        self.date = date
+        self.title = title
+        self.channel_name = channel_name

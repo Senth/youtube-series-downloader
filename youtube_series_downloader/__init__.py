dd6e4dc30f8192e39e4913cadd85655a20a1b152
Matteus Magnusson <senth.wallace@gmail.com>
2021-05-12 21:49:54 +0200

    🔥 Remove unnecessary info

--- youtube_series_downloader/__init__.py
+++ youtube_series_downloader/__init__.py
@@ -1,5 +1,1 @@
-"""youtube-series-downloader - Downloads YouTube series and optionally speeds them up to be watched on TVs"""
 
-__version__ = "0.1.0"
-__author__ = "Matteus Magnusson <senth.wallace@gmail.com>"
-__all__ = []

81f8fb6d6f5806ce2d313a6b488da6779ea4836a
Senth <senth.wallace@gmail.com>
2021-07-19 16:40:30 +0200

    🚧 🚚 Downloader -> youtube_dl
    Fixes #18

--- youtube_series_downloader/adapters/app_adapter.py
+++ youtube_series_downloader/adapters/app_adapter.py
@@ -1,25 +1,26 @@
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from youtube_series_downloader.app.download_new_episodes.download_new_episodes_repo import (
     DownloadNewEpisodesRepo,
 )
 from youtube_series_downloader.core.channel import Channel
 from youtube_series_downloader.core.video import Video
+from youtube_series_downloader.gateways.ffmpeg_gateway import FfmpegGateway
 from youtube_series_downloader.gateways.sqlite_gateway import SqliteGateway
+from youtube_series_downloader.gateways.youtube_dl_gateway import YoutubeDlGateway
 from youtube_series_downloader.gateways.youtube_gateway import YoutubeGateway
 
 
 class AppAdapter(DownloadNewEpisodesRepo):
     def __init__(self) -> None:
-        self.youtube_gateway = YoutubeGateway()
         self.sqlite_gateway = SqliteGateway()
         
     def close(self) -> None:
         self.sqlite_gateway.close()
     
     def get_latest_videos(self, channel: Channel) -> List[Video]:
-        return self.youtube_gateway.get_videos(channel)
+        return YoutubeGateway.get_videos(channel)
 
     def has_downloaded(self, video: Video) -> bool:
         return self.sqlite_gateway.has_downloaded(video.id)
@@ -27,8 +28,8 @@ class AppAdapter(DownloadNewEpisodesRepo):
     def set_as_downloaded(self, channel: Channel, video: Video) -> None:
         return self.sqlite_gateway.add_downloaded(channel.name, video.id)
     
-    def download(self, video: Video) -> Path:
-        return super().download(video)
+    def download(self, video: Video) -> Optional[Path]:
+        return YoutubeDlGateway.download(video)
     
-    def render(self, in: Path, out: Path, title: str, speed: float) -> None:
-        return super().render(in, out, title, speed)
+    def render(self, video: Video, in: Path, out: Path, speed: float) -> bool:
+        return FfmpegGateway.render(video, in, out, speed)
--- youtube_series_downloader/app/download_new_episodes/download_new_episodes_repo.py
+++ youtube_series_downloader/app/download_new_episodes/download_new_episodes_repo.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from youtube_series_downloader.core.channel import Channel
 from youtube_series_downloader.core.video import Video
@@ -15,8 +15,8 @@ class DownloadNewEpisodesRepo:
     def set_as_downloaded(self, channel: Channel, video: Video) -> None:
         raise NotImplementedError()
 
-    def download(self, video: Video) -> Path:
+    def download(self, video: Video) -> Optional[Path]:
         raise NotImplementedError()
 
-    def render(self, in: Path, out:Path, title: str, speed: float) -> None:
+    def render(self, video: Video, in: Path, out:Path, speed: float) -> bool:
         raise NotImplementedError()
